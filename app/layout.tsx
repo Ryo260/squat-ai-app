3dcfc7278f628c8b1a68fd67ed28f9333aa8bfce
ryo260 <biza1458@yahoo.co.jp>
2025-11-27 14:16:53 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
